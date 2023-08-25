e0ce6aeb4563fd59cd9a9d9a5dd0cdbd3d93ff61
Nihad-NA <testaccount@gmail.com>
2023-08-25 17:01:59 +0400

    file updated

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-CORS(app)
-
-API_KEY = "4b1a0e44-0205-4ad8-9158-3c10c3e87ae9"
 
 
-@app.route('/api/news', methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello_world():
-    api_key = request.args.get('api_key')
-
-    if api_key != API_KEY:
-        return jsonify({'error': 'Invalid API key'}), 401
-
     message = {"message": "Demo"}
     return jsonify(message)
 
